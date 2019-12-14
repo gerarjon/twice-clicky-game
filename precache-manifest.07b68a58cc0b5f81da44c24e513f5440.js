4d259e756ba3114118cd605151a67563dfa82f14
gerarjon <gerarsuaverdez@gmail.com>
2019-12-14 15:46:01 -0800

    Updates

---
+++ precache-manifest.07b68a58cc0b5f81da44c24e513f5440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a082c417975a3e87e43d06f3deb0d5b0",
+    "url": "/twice-clicky-game/index.html"
+  },
+  {
+    "revision": "514cc9cccdb750f970ad",
+    "url": "/twice-clicky-game/static/css/main.093a23b7.chunk.css"
+  },
+  {
+    "revision": "8e3416c7afdcd21e4e24",
+    "url": "/twice-clicky-game/static/js/2.967ca4c6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/twice-clicky-game/static/js/2.967ca4c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "514cc9cccdb750f970ad",
+    "url": "/twice-clicky-game/static/js/main.47aa7fae.chunk.js"
+  },
+  {
+    "revision": "eaf96f3b9001e349821a",
+    "url": "/twice-clicky-game/static/js/runtime-main.780908a4.js"
+  }
+]);
