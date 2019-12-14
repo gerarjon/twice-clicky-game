b657501dd8f76c441eeb045b165b43857483f659
gerarjon <gerarsuaverdez@gmail.com>
2019-12-14 15:51:29 -0800

    removed link in navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import "./Navbar.css";
 
 const Navbar = props => (
     <nav className="navbar sticky-top">
-        <a href="/" className="navbar-brand">Twice Clicky Game</a>
+        <span className="navbar-brand">Twice Clicky Game</span>
         <span className="navbar-text message">{props.message}</span>
         <span className="navbar-text">Score: {props.score} | Top Score: {props.topScore}</span>
     </nav>
