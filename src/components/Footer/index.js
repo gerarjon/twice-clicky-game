e639865d2fb64abe95428ff05dab0d5b0c2e140c
gerarjon <gerarsuaverdez@gmail.com>
2019-12-14 15:47:14 -0800

    updated link in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ const Footer = props => (
         {/* Copyright  */}
         <div class="footer-copyright text-center py-3">© 2019 Copyright: 
             <a href="https://github.com/gerarjon/"> Gerar Suaverdez </a>|
-            <a href="https://github.com/gerarjon/clicky-game"> GitHub Repo</a>
+            <a href="https://github.com/gerarjon/twice-clicky-game"> GitHub Repo</a>
         </div>
     </footer>
 )
